4c94028a76c74a8e7eb5032395588a2dbe387a33
Muhammad Ashfaq <48868012+muhammadashfaq@users.noreply.github.com>
2023-06-22 17:12:32 +0500

    boilderplate setup

---
+++ middlewares/multer.js
@@ -0,0 +1,15 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+ destination: function (_req, _file, cb) {
+  cb(null, "uploads");
+ },
+ filename: function (_req, file, cb) {
+  cb(null, Date.now() + path.extname(file.originalname));
+ },
+});
+
+const upload = multer({storage: storage});
+
+module.exports = upload;
