74e4098fbc646994100d07ef3b9bcadc02786e56
Muhammad Ashfaq <mobile@viralsquare.org>
2023-08-18 19:32:18 +0500

    added try/catch in location controller

--- controllers/location/locationControllers.js
+++ controllers/location/locationControllers.js
@@ -3,48 +3,48 @@ const catchAsync = require('../../utils/catchAsync');
 const prisma = require('../../prisma');
 
 exports.createRequest = catchAsync(async (req, res, next) => {
-  //   try {
-  const { lat, lang, device_id } = req?.body;
+  try {
+    const { lat, lang, device_id } = req?.body;
 
-  if (!lat || !lang || !device_id) {
-    return next(
-      new AppError(
-        !lat
-          ? 'Latitude is required!'
-          : !lang
-          ? 'Longitude is required!'
-          : 'Device id is required',
-        400,
-        res,
-      ),
-    );
-  }
+    if (!lat || !lang || !device_id) {
+      return next(
+        new AppError(
+          !lat
+            ? 'Latitude is required!'
+            : !lang
+            ? 'Longitude is required!'
+            : 'Device id is required',
+          400,
+          res,
+        ),
+      );
+    }
 
-  const check = await prisma.device.findFirst({
-    where: { id: device_id },
-  });
+    const check = await prisma.device.findFirst({
+      where: { id: device_id },
+    });
 
-  if (!check) {
-    return next(new AppError('Device not found!', 400, res));
-  }
+    if (!check) {
+      return next(new AppError('Device not found!', 400, res));
+    }
 
-  const result = await prisma.location.create({
-    data: {
-      latitude: lat,
-      longitude: lang,
-      device: {
-        connect: { id: device_id },
+    const result = await prisma.location.create({
+      data: {
+        latitude: lat,
+        longitude: lang,
+        device: {
+          connect: { id: device_id },
+        },
       },
-    },
-  });
+    });
 
-  res.status(200).json({
-    status: 'success',
-    data: result,
-  });
-  //   } catch (error) {
-  //     const { message, statusCode } = error;
-  //     console.log('statusCode:=>  ', error);
-  //     return next(new AppError(message, statusCode, res));
-  //   }
+    res.status(200).json({
+      status: 'success',
+      data: result,
+    });
+  } catch (error) {
+    const { message, statusCode } = error;
+    console.log('statusCode:=>  ', error);
+    return next(new AppError(message, statusCode, res));
+  }
 });
